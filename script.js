1377ad1a55339b4badbf58cc7a86825e46119fd9
Luiz Phelipe Thomaz Nascimento <109628899+luizphthomaz@users.noreply.github.com>
2025-03-28 10:58:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,21 +3,69 @@ const btnFoco = document.querySelector('.app__card-button--foco')
 const btnCurto = document.querySelector('.app__card-button--curto')
 const btnLongo = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+
+// salvando o áudio em uma variável
+const musica = new Audio('./sons/luna-rise-part-one.mp3')
+
+// para o aúdio ser tocado o tempo todo
+musica.loop = true
+
+// quando o input checkbox for acionado (trocar de estado)
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
 
 btnFoco.addEventListener('click', () => {
 
-    //  
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', './imagens/foco.png')
+    alterarContexto('foco')
+    btnFoco.classList.add('active')
 })
 
 btnCurto.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', './imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
+    btnCurto.classList.add('active')
 })
 
 btnLongo.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', './imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
+    btnLongo.classList.add('active')
 })
 
+function alterarContexto(contexto) {
+    botoes.forEach(function(contexto) {
+        contexto.classList.remove('active')
+    })
+
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+    
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+        break;
+
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta.</strong>`
+
+        break;
+
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície,<br>
+                <strong class="app__title-strong">faça uma pausa longa.</strong>`
+
+        break;
+    
+        default:
+        break;
+    }
+}
