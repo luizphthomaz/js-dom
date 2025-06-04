a3f4a79f650187f2d213a54065baf0b19156f2b9
Luiz Phelipe Thomaz Nascimento <luizwell88@gmail.com>
2025-06-03 21:55:20 -0300

    pegando tarefas do localStorage

--- script-crud.js
+++ script-crud.js
@@ -3,7 +3,7 @@ const formularioTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list"); // Seleciona a lista de tarefas
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] // Recupera as tarefas do localStorage ou inicializa como um array vazio;
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] // Recupera as tarefas do localStorage (inverso do stringfy) ou inicializa como um array vazio;
 
 function criarTarefa(tarefa) {
     const li = document.createElement("li"); // Cria um elemento <li>
@@ -11,16 +11,18 @@ function criarTarefa(tarefa) {
 
     const svg = document.createElement("svg");
     svg.innerHTML = `
-        <svg>
-            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        
+            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"  xmlns="http://www.w3.org/2000/svg">
                 <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
                 <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
             </svg>
-        </svg>
+        
     `
 
     const paragrafo = document.createElement("p");
     paragrafo.textContent = tarefa.descricao; // Define o texto do parágrafo como a descrição da tarefa
+    paragrafo.classList.add("app__section-task-list-item-description");
+
 
     const botao = document.createElement("button");
     botao.classList.add("app_button-edit");   
@@ -31,6 +33,8 @@ function criarTarefa(tarefa) {
 
     botao.append(imagemBotao)
     li.append(svg, paragrafo, botao); // Adiciona o SVG, o parágrafo e o botão ao <li>
+
+    return li; // Retorna o elemento <li> completo
 }
 
 btnAdicionarTarefa.addEventListener("click", () => {
@@ -49,11 +53,16 @@ formularioTarefa.addEventListener("submit", (event) => {
     tarefas.push(tarefa); // Adiciona a tarefa ao array de tarefas
     textarea.value = ""; // Limpa o campo de texto
     
-    localStorage.setItem('tarefas', JSON.stringify(tarefas)) // Salva as tarefas no localStorage
+    const elementoTarefa = criarTarefa(tarefa); // Cria o elemento de tarefa
+    ulTarefas.append(elementoTarefa); // Adiciona o elemento de tarefa à lista de tarefas
+    localStorage.setItem('tarefas', JSON.stringify(tarefas)) // Salva as tarefas no localStorage 
+    textarea.value = ""; // Limpa o campo de texto após adicionar a tarefa
+    formularioTarefa.classList.add("hidden"); // Esconde o formulário de adicionar tarefa
 })
 
 // para cada tarefa no array de tarefas, cria um elemento de tarefa e adiciona à lista de tarefas
 tarefas.forEach(tarefa => {
-    const elemetoTarefa = criarTarefa(tarefa); // Cria a tarefa na lista
-    ulTarefas.append(elemetoTarefa); // Adiciona a tarefa à lista de tarefas
+    const elementoTarefa = criarTarefa(tarefa); // Cria a tarefa na lista
+    ulTarefas.append(elementoTarefa); // Adiciona a tarefa à lista de tarefas
 })
+ 
